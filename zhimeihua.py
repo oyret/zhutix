8757d44170fe6e45a6c64ec63c00ca34996baf7d
oyret <69465910+oyret@users.noreply.github.com>
2023-04-02 18:32:43 +0800

    Update zhimeihua.py

--- zhimeihua.py
+++ zhimeihua.py
@@ -24,77 +24,77 @@ def main():
     browser = webdriver.Chrome(service=Service(chrome_dirver_path), options=chrome_option)
     browser.get(url)
     browser.maximize_window()
-    time.sleep(random.randint(3, 6))
+    time.sleep(random.randint(5, 10))
     # 点击登录图像
     browser.find_element(by=By.CLASS_NAME, value='login-button').click()
-    time.sleep(random.randint(3, 6))
+    time.sleep(random.randint(5, 10))
     # 点击账号登录
     browser.find_element(by=By.XPATH, value='//div[@class="dlann"]/div[4]').click()
-    time.sleep(random.randint(3, 6))
+    time.sleep(random.randint(5, 10))
     # 输入用户名
     browser.find_element(by=By.XPATH, value='//div[@id="zhdlk"]/div/label[2]/input').send_keys(username)
-    time.sleep(random.randint(3, 6))
+    time.sleep(random.randint(5, 10))
     # 输入密码
     browser.find_element(by=By.XPATH, value='//div[@id="zhdlk"]/div/label[5]/input').send_keys(password)
-    time.sleep(random.randint(3, 6))
+    time.sleep(random.randint(5, 10))
     # 点击快速登录
     browser.find_element(by=By.XPATH, value='//div[@id="zhdlk"]/div/div[3]/button').click()
-    time.sleep(random.randint(3, 6))
+    time.sleep(random.randint(5, 10))
     
     # 点击头像
     browser.find_element(by=By.XPATH, value='//div[@class="top-user-avatar avatar-parent"]/img').click()
-    time.sleep(random.randint(3, 6))
+    time.sleep(random.randint(5, 10))
     # 点击任务中心
     browser.find_element(by=By.XPATH, value='//ul/li[3]/a/p/span').click()
-    time.sleep(random.randint(3, 6))
+    time.sleep(random.randint(5, 10))
     # 点击签到   
     browser.find_element(by=By.XPATH, value='//*[@id="main"]/div[2]/div[1]/div[2]/ul/li[4]/a').click()
-    time.sleep(random.randint(3, 6))
+    time.sleep(random.randint(5, 10))
     # 点击关注某人
     browser.find_element(by=By.XPATH, value='//*[@id="main"]/div[2]/div[1]/div[2]/ul/li[3]/a').click()
-    time.sleep(random.randint(3, 6))   
+    time.sleep(random.randint(5, 10))   
     ## 点击关注
     while browser.find_element(by=By.XPATH, value='//div[@id="primary-home"]/div/div/div/div/label/input').get_attribute('value') < re.search('\d{1,6}', browser.find_element(by=By.XPATH, value="//div[@id='primary-home']/div/div/div/div/label").text)[0]: # 当前页码 < 总页码
         flag = 0 # 是否点击关注的标志
-        for i in range(1,21):
-            if browser.find_element(by=By.XPATH, value='//ul/li[{}]/div/div[5]/button[1]'.format(i)).text!='已关注':      
-                browser.find_element(by=By.XPATH, value='//ul/li[{}]/div/div[5]/button[1]'.format(i)).click()
+        for i in range(1,17):
+            if browser.find_element(by=By.XPATH, value='//ul/li[{}]/div/div[4]/button[1]'.format(i)).text!='已关注':      
+                browser.find_element(by=By.XPATH, value='//ul/li[{}]/div/div[4]/button[1]'.format(i)).click()
                 flag = 1
-                time.sleep(random.randint(3, 6))
+                time.sleep(random.randint(5, 10))
                 break   
             time.sleep(random.randint(2, 3))  
         if flag == 0:
-            browser.find_element(by=By.XPATH, value='//*[@id="primary-home"]/div[3]/div/div/div[2]/a[2]').click() # 下一页
+            browser.find_element(by=By.XPATH, value='//*[@id="primary-home"]/div[2]/div/div/div[2]/a[2]').click() # 下一页
             time.sleep(random.randint(2, 3))  
         else:
             ## 返回任务中心
             for j in range(int(browser.find_element(by=By.XPATH, value='//div[@id="primary-home"]/div/div/div/div/label/input').get_attribute('value'))):  # 页面后退次数由当前页码确定
                 browser.back()
-                time.sleep(random.randint(3, 6))
+                time.sleep(random.randint(5, 10))
             break
 
     for i in range(3):
         # 点击评论
         browser.find_element(by=By.XPATH, value='//*[@id="main"]/div[2]/div[1]/div[2]/ul/li[2]/a').click()
-        time.sleep(random.randint(3, 6))
+        time.sleep(random.randint(5, 10))
         ## 评论
-        browser.find_element(by=By.XPATH, value='//div[@id="comments"]/div/a').click()
-        time.sleep(random.randint(3, 6))
-        browser.find_element(by=By.XPATH, value='//div["respond"]/div[2]/div[3]/textarea[@id="textarea"]').click()
-        time.sleep(random.randint(3, 6))
-        browser.find_element(by=By.XPATH, value='//div["respond"]/div[2]/div[3]/textarea[@id="textarea"]').send_keys(random.choice(comments))
-        time.sleep(random.randint(3, 6))
-        browser.find_element(by=By.XPATH, value='//*[@id="respond"]/div[2]/div[4]/div[2]/button[2]').click()
-        time.sleep(random.randint(3, 6))
+        # browser.find_element(by=By.XPATH, value='//div[@id="comments"]/div/a').click()
+        # time.sleep(random.randint(5, 10))
+        browser.find_element(by=By.XPATH, value='//div["respond"]/div[3]/div[2]/div[2]/textarea[@id="textarea"]').click()
+        time.sleep(random.randint(5, 10))
+        browser.find_element(by=By.XPATH, value='//div["respond"]/div[3]/div[2]/div[2]/textarea[@id="textarea"]').send_keys(random.choice(comments))
+        time.sleep(random.randint(5, 10))
+        browser.find_element(by=By.XPATH, value='//*[@id="respond"]/div[3]/div[3]/div[2]/button[2]').click()
+        time.sleep(random.randint(5, 10))
         
         ## 返回任务中心
         browser.back()
-        time.sleep(random.randint(3, 6))
-        browser.back()
-        time.sleep(random.randint(3, 6))
+        time.sleep(random.randint(5, 10))
+        # browser.back()
+        # time.sleep(random.randint(5, 10))
         # 刷新页面
         browser.refresh()
-        time.sleep(random.randint(3, 6))
+        time.sleep(random.randint(5, 10))
     print("签到成功......")
     browser.quit()
      
@@ -106,5 +106,3 @@ if __name__ == '__main__':
     url = 'https://zhutix.com/'
     comments = ['很漂亮，我收藏了。','好好看，快来下载。','绝了，符合我的审美。','真不错，很喜欢哦。','真好看啊，强烈推荐。','好看好看，绝绝子。','看着非常不错，试试看。','谢谢分享，嘻嘻嘻。','已下载，真是太好看了。']
     main()
-    
-    
